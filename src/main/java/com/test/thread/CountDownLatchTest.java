1deade865aed6a5b398ba446b67fca48f0ae3d2c
zhengyi <zhengyi@fishcredit.net>
2019-04-16 15:00:54 +0800

    no message

--- src/main/java/com/test/thread/CountDownLatchTest.java
+++ src/main/java/com/test/thread/CountDownLatchTest.java
@@ -1,220 +1,117 @@
 package com.test.thread;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchTest {
-		public static void  main(String[] args) {
-			//testWaitThread();
-			testConcurrent();
-			   }
+	public static void main(String[] args) {
+		// testWaitThread();
+		testConcurrent();
+	}
 
+	/**
+	 * 1、模拟所有子线程都执行完成后再执行主线程 countdownLatch计数，模拟子线程执行完成之后再执行主线程 这个也可以用future来实现
+	 *
+	 */
+	public static void testWaitThread() {
+		final CountDownLatch latch = new CountDownLatch(2);
+		new Thread() {
+			public void run() {
+				try {
+					System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
 
-			/**
+					Thread.sleep(3000);
 
-			* 1、模拟所有子线程都执行完成后再执行主线程
+					System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
 
-			* countdownLatch计数，模拟子线程执行完成之后再执行主线程
+					latch.countDown();
 
-			* 这个也可以用future来实现
+				} catch (InterruptedException e) {
 
-			*
+					e.printStackTrace();
 
-			*/
+				}
 
-			publicstaticvoidtestWaitThread()
+			};
 
-			{
+		}.start();
 
-			final CountDownLatchlatch=newCountDownLatch(2);
+		new Thread() {
+			public void run() {
+				try {
+					System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+					Thread.sleep(3000);
+					System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+					latch.countDown();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			};
+		}.start();
+		try {
+			System.out.println("等待2个子线程执行完毕...");
+			latch.await();
+			System.out.println("2个子线程已经执行完毕");
+			System.out.println("继续执行主线程");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-			newThread(){
+	/**
+	 * 线程数量
+	 */
+	public static final int THREAD_NUM = 100;
 
-			     publicvoidrun() {
+	/**
+	 * 开始时间
+	 */
+	private static long startTime = 0L;
 
-			       try{
-
-			         System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
-
-			        Thread.sleep(3000);
-
-			        System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
-
-			        latch.countDown();
-
-			      }catch(InterruptedExceptione) {
-
-			        e.printStackTrace();
-
-			      }
-
-			     };
-
-			   }.start();
-
-
-
-			   newThread(){
-
-			     publicvoidrun() {
-
-			       try{
-
-			         System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
-
-			         Thread.sleep(3000);
-
-			         System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
-
-			         latch.countDown();
-
-			      }catch(InterruptedExceptione) {
-
-			        e.printStackTrace();
-
-			      }
-
-			     };
-
-			   }.start();
-
-
-
-			   try{
-
-			     System.out.println("等待2个子线程执行完毕...");
-
-			    latch.await();
-
-			    System.out.println("2个子线程已经执行完毕");
-
-			    System.out.println("继续执行主线程");
-
-			  }catch(InterruptedExceptione) {
-
-			    e.printStackTrace();
-
-			  }
-
+	/**
+	 * 2、模拟高并发
+	 */
+	public static void testConcurrent() {
+		try {
+			startTime = System.currentTimeMillis();
+			System.out.println("CountDownLatch started at: " + startTime);
+			// 初始化计数器为1
+			CountDownLatch countDownLatch = new CountDownLatch(1);
+			for (int i = 0; i < THREAD_NUM; i++) {
+				new Thread(new Run(countDownLatch)).start();
 			}
 
+			// 启动多个线程
+			countDownLatch.countDown();
 
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+	}
 
-			/**
+	/**
+	 * 线程类
+	 */
+	private static class Run implements Runnable {
+		private final CountDownLatch startLatch;
 
-			 * 线程数量
+		public Run(CountDownLatch startLatch) {
+			this.startLatch = startLatch;
+		}
 
-			 */
-
-			 publicstaticfinalintTHREAD_NUM= 100;
-
-
-
-			 /**
-
-			 * 开始时间
-
-			 */
-
-			 privatestaticlongstartTime= 0L;
-
-			 /**
-
-			 * 2、模拟高并发
-
-			 */
-
-			publicstaticvoidtestConcurrent()
-
-			{
-
-
-			 try{
-
-			     startTime= System.currentTimeMillis();
-
-			     System.out.println("CountDownLatch started at: "+startTime);
-
-
-
-			     // 初始化计数器为1
-
-			     CountDownLatchcountDownLatch=newCountDownLatch(1);
-
-
-
-			     for(inti= 0;i<THREAD_NUM;i++) {
-
-			       newThread(newRun(countDownLatch)).start();
-
-			     }
-
-
-
-			     // 启动多个线程
-
-			     countDownLatch.countDown();
-
-
-
-			   }catch(Exceptione) {
-
-			     System.out.println("Exception: "+e);
-
-			   }
-
+		@Override
+		public void run() {
+			try {
+				// 线程等待
+				startLatch.await();
+				// 模拟耗时操作
+				Thread.sleep(3000);
+				long endTime = System.currentTimeMillis();
+				System.out
+						.println(Thread.currentThread().getName() + " ended at: " + endTime + ", cost: " + (endTime - startTime) + " ms.");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
-
-			/**
-
-			 * 线程类
-
-			 */
-
-			 privatestaticclassRunimplementsRunnable {
-
-			   privatefinalCountDownLatchstartLatch;
-
-
-
-			   publicRun(CountDownLatchstartLatch) {
-
-			     this.startLatch=startLatch;
-
-			   }
-
-
-
-			   @Override
-
-			   publicvoidrun() {
-
-			     try{
-
-			       // 线程等待
-
-			       startLatch.await();
-
-
-
-			       // 模拟耗时操作
-
-			       Thread.sleep(3000);
-
-
-
-			       longendTime= System.currentTimeMillis();
-
-			       System.out.println(Thread.currentThread().getName() +" ended at: "+endTime+", cost: "+ (endTime-startTime) +" ms.");
-
-			     }catch(Exceptione) {
-
-			       e.printStackTrace();
-
-			     }
-
-
-
-			   }
-
-		
+		}
 	}
 }
