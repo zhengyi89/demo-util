d7d5fc2a6abf0654f72580ed4404f9a0e45b73d8
zhengyi <zhengyi@wsmtec.com>
2019-10-16 18:26:37 +0800

    模板模式定义、结构与样例

---
+++ design-pattern/src/main/java/com/test/template/sample/StudyAbroadProcess.java
@@ -0,0 +1,12 @@
+package com.test.template.sample;
+
+/**
+ * @Author: zhengyi
+ * @Date: 2019/10/16 17:51
+ */
+public class StudyAbroadProcess {
+    public static void main(String[] args) {
+        StudyAbroad tm = new StudyInAmerica();
+        tm.TemplateMethod();
+    }
+}
