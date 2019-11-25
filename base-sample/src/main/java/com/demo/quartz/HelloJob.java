3a053e7cf66d8b990e50e5f240768281723b5dcf
zhengyi <zhengyi@wsmtec.com>
2019-11-25 10:36:00 +0800

    提交修改

--- base-sample/src/main/java/com/demo/quartz/HelloJob.java
+++ base-sample/src/main/java/com/demo/quartz/HelloJob.java
@@ -8,12 +8,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HelloJob implements Job {
-	public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-		// 打印当前的执行时间 例如 2017-11-23 00:00:00
-		Date date = new Date();
-		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("现在的时间是：" + sf.format(date));
-		// 具体的业务逻辑
-		System.out.println("Hello Quartz");
-	}
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 打印当前的执行时间 例如 2017-11-23 00:00:00
+        Date date = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("现在的时间是：" + sf.format(date));
+        // 具体的业务逻辑
+        System.out.println("Hello Quartz");
+    }
 }
