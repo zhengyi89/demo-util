b1c27fe949d92bb91cc0c9b4cd7fe48bcf188c50
zhengyi <zhengyi@wsmtec.com>
2019-12-03 11:39:16 +0800

    spring 事件机制

---
+++ springboot-sample/src/main/java/com/demo/spring/ApplicationListenerSample.java
@@ -0,0 +1,26 @@
+package com.demo.spring;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * application context 事件机制
+ *
+ * @Author: zhengyi
+ * @Date: 2019/11/28 13:30
+ */
+@Component
+public class ApplicationListenerSample implements ApplicationListener<ContextRefreshedEvent> {
+
+    /**
+     * applicationContext初始化或刷新时，该事件发布
+     *
+     * @param contextRefreshedEvent
+     */
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println(contextRefreshedEvent);
+        System.out.println("ApplicationListenerSample............................");
+    }
+}
---
+++ springboot-sample/src/main/java/com/demo/spring/customerApplicationEvent/EmailNotifier.java
@@ -0,0 +1,34 @@
+package com.demo.spring.customerApplicationEvent;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * 自定义监听
+ *
+ * @Author: zhengyi
+ * @Date: 2019/12/3 11:27
+ */
+
+public class EmailNotifier implements ApplicationListener {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+
+        if (event instanceof EmailEvent) {
+
+            EmailEvent emailEvent = (EmailEvent) event;
+
+            System.out.println("邮件地址：" + emailEvent.getAddress());
+
+            System.out.println("邮件内容：" + emailEvent.getText());
+
+        } else {
+
+            System.out.println("容器本身事件：" + event);
+
+        }
+
+    }
+
+}
