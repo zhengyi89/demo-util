aa471faf81e41a797bba0829909e3b9b124681c2
zhengyi <zhengyi@wsmtec.com>
2019-12-12 16:43:28 +0800

    提交修改

--- base-sample/src/main/java/com/sample/assertTest/TestMain.java
+++ base-sample/src/main/java/com/sample/assertTest/TestMain.java
@@ -2,10 +2,10 @@ package com.sample.assertTest;
 
 public class TestMain {
 
-	public static void main(String[] args) {
-		 boolean isOk = 1>2;
-	           assert isOk;
-	           System.out.println("程序正常");
-		}
+    public static void main(String[] args) {
+        boolean isOk = 1 > 2;
+        assert isOk;
+        System.out.println("程序正常");
+    }
 
 }
